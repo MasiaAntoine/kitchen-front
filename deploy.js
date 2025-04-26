e453fa40ce54a86803171ead60681d903d5a0f78
ThorKild_ <pro.antoine.masia@gmail.com>
2025-04-27 00:41:18 +0200

    Ajout de la création du fichier .htaccess lors du déploiement

--- deploy.js
+++ deploy.js
@@ -1,6 +1,7 @@
 import { execSync } from 'child_process'
 import path from 'path'
 import dotenv from 'dotenv'
+import fs from 'fs'
 
 dotenv.config()
 
@@ -16,12 +17,34 @@ function run(command, message) {
   execSync(command, { stdio: 'inherit' })
 }
 
+function createHtaccess() {
+  console.log('\x1b[36m› Creating .htaccess file...\x1b[0m')
+  const htaccessContent = `<IfModule mod_rewrite.c>
+  RewriteEngine On
+  RewriteBase /
+  RewriteRule ^index\.html$ - [L]
+  RewriteCond %{REQUEST_FILENAME} !-f
+  RewriteCond %{REQUEST_FILENAME} !-d
+  RewriteRule . /index.html [L]
+</IfModule>`
+
+  fs.writeFileSync(path.join(DIST_DIR, '.htaccess'), htaccessContent)
+}
+
 try {
   run('pnpm build', 'Building Vue app')
+  createHtaccess()
+
   run(
-    `scp -i ${SSH_KEY_PATH} -P ${REMOTE_PORT} -r ${DIST_DIR}/* ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}`,
+    `ssh -i ${SSH_KEY_PATH} -p ${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST} "rm -rf ${REMOTE_PATH}/*"`,
+    'Cleaning old files on server',
+  )
+
+  run(
+    `scp -i ${SSH_KEY_PATH} -P ${REMOTE_PORT} -r ${DIST_DIR}/* ${DIST_DIR}/.htaccess ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}`,
     'Deploying to Hostinger',
   )
+
   console.log('\x1b[32m✔ Deployment successful!\x1b[0m')
 } catch (err) {
   console.error('\x1b[31m✖ Deployment failed:\x1b[0m', err.message)
